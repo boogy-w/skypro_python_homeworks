518fb3fa28b29814c57df39ce1317eab3f82b4d6
boogy-w <wboogy1@gmail.com>
2024-09-25 13:43:46 +0300

    add first homework

---
+++ lesson2/esson_2_task_0.py
@@ -0,0 +1,5 @@
+my_age = input("Введите ваш возраст: ")
+my_age = int(my_age)
+print(f"Ваш возраст: {my_age}")
+my_age = my_age + 1
+print(f"Обновленный возраст: {my_age}")
---
+++ lesson2/esson_2_task_7.py
@@ -0,0 +1,3 @@
+nlst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+total_sum = sum(nlst)
+print(total_sum)
